334764a584f5bca967ee03871347d034fdacc14f
Tim C <tim.crutchley@gmail.com>
2023-04-16 10:54:58 -0600

    Node Exercise 1 - Basics

---
+++ index.js
@@ -0,0 +1,8 @@
+// April 15, 2023
+// Node.JS Crash Course
+
+const Person = require('./person');
+
+const person1 = new Person('John Doe', 30);
+
+person1.greeting();
---
+++ person.js
@@ -0,0 +1,23 @@
+// const person = {
+//     name: 'John Doe',
+//     age: 30
+// }
+
+// Module Wrapper Function
+// This wraps around the person.js module to give us acccess to exports, require, module
+(function (exports, require, module, __filename, __dirname) {
+
+})
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age}`)
+    }
+}
+
+module.exports = Person;
